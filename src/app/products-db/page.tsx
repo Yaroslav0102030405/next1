6bf1802b79adaa3d4337a2e845780b78c39f604f
Yaroslav Lukovetskyi <111358303+Yaroslav0102030405@users.noreply.github.com>
2025-06-28 20:48:43 +0300

    useOptimistic

--- src/app/products-db/page.tsx
+++ src/app/products-db/page.tsx
@@ -1,6 +1,7 @@
 import { getProducts } from "@/prisma-db";
-import Link from "next/link";
-import { removeProduct } from "../actions/products";
+// import Link from "next/link";
+// import { removeProduct } from "../actions/products";
+import ProductDetail from "./product-detail";
 
 export type Product = {
   id: number;
@@ -12,22 +13,23 @@ export default async function ProductsPrismaDBPage() {
   const products: Product[] = await getProducts();
 
   return (
-    <ul className="space-y-4 p-4">
-      {products.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-xl font-semibold">
-            <Link href={`/products-db/${product.id}`}>{product.image}</Link>    
-          </h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">{product.title}</p>
-         <form action={removeProduct.bind(null, product.id)}>
-           <button type="submit" className="px-4 py-2 mt-4 text-white bg-red-500 hover:bg-red-800 rounded-md cursor-pointer">Видалити</button>
-         </form>
-        </li>
-      ))}
-    </ul>
+    // <ul className="space-y-4 p-4">
+    //   {products.map((product) => (
+    //     <li
+    //       key={product.id}
+    //       className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+    //     >
+    //       <h2 className="text-xl font-semibold">
+    //         <Link href={`/products-db/${product.id}`}>{product.image}</Link>    
+    //       </h2>
+    //       <p>{product.description}</p>
+    //       <p className="text-lg font-medium">{product.title}</p>
+    //      <form action={removeProduct.bind(null, product.id)}>
+    //        <button type="submit" className="px-4 py-2 mt-4 text-white bg-red-500 hover:bg-red-800 rounded-md cursor-pointer">Видалити</button>
+    //      </form>
+    //     </li>
+    //   ))}
+    // </ul>
+    <ProductDetail products={products} />
   );
 }
